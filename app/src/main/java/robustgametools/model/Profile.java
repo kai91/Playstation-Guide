3931630b09f0bed82c3c31fbf2869ea04c22ff6c
Kai <wngkai.91@gmail.com>
2014-10-17 18:03:51 -0700

    Added Profile class to keep track of player's profile

---
+++ app/src/main/java/robustgametools/model/Profile.java
@@ -0,0 +1,85 @@
+package robustgametools.model;
+
+/**
+ * Profile to store the information about user's account.
+ *
+ * @author kai
+ */
+public class Profile {
+
+    private String onlineId, avatarUrl;
+    private int level, progress, platinum, gold, silver, bronze;
+    private boolean plus;
+
+    public String getOnlineId() {
+        return onlineId;
+    }
+
+    public void setOnlineId(String onlineId) {
+        this.onlineId = onlineId;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public void setProgress(int progress) {
+        this.progress = progress;
+    }
+
+    public int getPlatinum() {
+        return platinum;
+    }
+
+    public void setPlatinum(int platinum) {
+        this.platinum = platinum;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public int getSilver() {
+        return silver;
+    }
+
+    public void setSilver(int silver) {
+        this.silver = silver;
+    }
+
+    public int getBronze() {
+        return bronze;
+    }
+
+    public void setBronze(int bronze) {
+        this.bronze = bronze;
+    }
+
+    public boolean isPlus() {
+        return plus;
+    }
+
+    public void setPlus(boolean plus) {
+        this.plus = plus;
+    }
+}
