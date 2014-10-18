4a3ed6510dda5d8011c00fb3e5f98bd2bdfefcc5
Kai <wngkai.91@gmail.com>
2014-10-17 18:04:31 -0700

    Added JsonFactory class to process Json (To be implemented)

---
+++ app/src/main/java/robustgametools/util/JsonFactory.java
@@ -0,0 +1,11 @@
+package robustgametools.util;
+
+/**
+ * JsonFactory utility class to convert Json into
+ * class object and vice versa. Also used for
+ * parsing purposes
+ *
+ * @author kai
+ */
+public class JsonFactory {
+}
