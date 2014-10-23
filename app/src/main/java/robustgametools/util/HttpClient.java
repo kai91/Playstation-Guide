1a116af1fb78922e8572327e1a266200fce2dedc
Kai <wngkai.91@gmail.com>
2014-10-23 13:11:00 -0700

    Fixed cancelRequests bug

--- app/src/main/java/robustgametools/util/HttpClient.java
+++ app/src/main/java/robustgametools/util/HttpClient.java
@@ -1,21 +1,27 @@
 package robustgametools.util;
 
+import android.content.Context;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestHandle;
+
+import java.util.ArrayList;
 
 /**
  * HttpClient to process all http requests for the application
  */
 public class HttpClient {
 
-    private static AsyncHttpClient asyncHttpClient = null;
+    private static AsyncHttpClient mAsyncHttpClient = null;
+    private static ArrayList<RequestHandle> mRequestHandles = new ArrayList<RequestHandle>();
 
     // This is the domain name/ip address of the server
     private static String serverUrl = "http://boiling-bastion-9577.herokuapp.com/";
 
     private static void init() {
-        if (asyncHttpClient == null) {
-            asyncHttpClient = new AsyncHttpClient();
+        if (mAsyncHttpClient == null) {
+            mAsyncHttpClient = new AsyncHttpClient();
         }
     }
 
@@ -29,15 +35,18 @@ public class HttpClient {
     public static void signIn(String username, AsyncHttpResponseHandler responseHandler) {
         init();
         String url = serverUrl + "psn/" + username;
-        asyncHttpClient.get(url, null, responseHandler);
+        mRequestHandles.add(mAsyncHttpClient.get(url, null, responseHandler));
     }
 
     /**
      * Cancel all ongoing requests
      */
     public static void cancelRequests() {
-        if (asyncHttpClient != null) {
-            asyncHttpClient.cancelAllRequests(true);
+        if (!mRequestHandles.isEmpty()) {
+            int size = mRequestHandles.size();
+            for (int j = 0; j < size; j++ ) {
+                mRequestHandles.get(j).cancel(true);
+            }
         }
     }
 }
