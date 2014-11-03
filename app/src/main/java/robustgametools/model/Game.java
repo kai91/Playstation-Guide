943447a082f09510a9199e56bd57415383246199
Kai <wngkai.91@gmail.com>
2014-11-02 19:44:14 -0800

    Updated Game.java field variables to reflect info in JSON

--- app/src/main/java/robustgametools/model/Game.java
+++ app/src/main/java/robustgametools/model/Game.java
@@ -10,6 +10,13 @@ public class Game {
     private String mTitle;
     private Platform mPlatform;
     private ArrayList<Trophy> mTrophies;
+    private boolean mHasDlc;
+    private String mNpCommunicationId;
+    private String mTrophyIconUrl;
+    private int mBronze, mSilver, mGold, mPlatinum;
+
+    public Game() {
+    }
 
     public String getTitle() {
         return mTitle;
@@ -33,5 +40,61 @@ public class Game {
 
     public void setTrophies(ArrayList<Trophy> mTrophies) {
         this.mTrophies = mTrophies;
+    }
+
+    public boolean hasDlc() {
+        return mHasDlc;
+    }
+
+    public void setHasDlc(boolean mHasDlc) {
+        this.mHasDlc = mHasDlc;
+    }
+
+    public String getNpCommunicationId() {
+        return mNpCommunicationId;
+    }
+
+    public void setNpCommunicationId(String mNpCommunicationId) {
+        this.mNpCommunicationId = mNpCommunicationId;
+    }
+
+    public String getTrophyIconUrl() {
+        return mTrophyIconUrl;
+    }
+
+    public void setTrophyIconUrl(String mTrophyTitleIconUrl) {
+        this.mTrophyIconUrl = mTrophyTitleIconUrl;
+    }
+
+    public int getBronze() {
+        return mBronze;
+    }
+
+    public void setBronze(int mBronze) {
+        this.mBronze = mBronze;
+    }
+
+    public int getSilver() {
+        return mSilver;
+    }
+
+    public void setSilver(int mSilver) {
+        this.mSilver = mSilver;
+    }
+
+    public int getGold() {
+        return mGold;
+    }
+
+    public void setGold(int mGold) {
+        this.mGold = mGold;
+    }
+
+    public int getPlatinum() {
+        return mPlatinum;
+    }
+
+    public void setPlatinum(int mPlatinum) {
+        this.mPlatinum = mPlatinum;
     }
 }
