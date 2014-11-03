33669b4e28b506bffd7836c234825d6f406104ec
Kai <wngkai.91@gmail.com>
2014-11-02 19:38:36 -0800

    Updated JsonFactory to parse for user profile data and game data

--- app/src/main/java/robustgametools/util/JsonFactory.java
+++ app/src/main/java/robustgametools/util/JsonFactory.java
@@ -1,12 +1,14 @@
 package robustgametools.util;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.util.ArrayList;
 
 import robustgametools.model.Game;
+import robustgametools.model.Platform;
 import robustgametools.model.Profile;
 
 /**
@@ -33,10 +35,50 @@ public class JsonFactory {
         return mJsonFactory;
     }
 
-    public ArrayList<Game> getGames(String gameJson) {
-        return null;
+    /**
+     * Initialized a new array of games and parsed the info.
+     * @param gameJson - Information in JSON retrieved from server
+     * @return
+     */
+    public ArrayList<Game> parseGames(String gameJson) {
+        return parseGames(gameJson, new ArrayList<Game>());
     }
 
+    /**
+     * Parsed info and add into existing game array
+     * @param gameJson - Information in JSON retrieved from server
+     * @param gameArray - Existing array of games to be appended to
+     * @return
+     */
+    public ArrayList<Game> parseGames(String gameJson, ArrayList<Game> gameArray) {
+        JsonObject jsonObject = new JsonParser().parse(gameJson).getAsJsonObject();
+        JsonArray array = jsonObject.get("trophyTitles").getAsJsonArray();
+        for (int i = array.size()-1; i >= 0 ; i--) {
+            JsonObject jsGame = array.get(i).getAsJsonObject();
+            Game game = new Game();
+            game.setNpCommunicationId(jsGame.get("npCommunicationId").getAsString());
+            game.setTitle(jsGame.get("trophyTitleName").getAsString());
+            game.setTrophyIconUrl(jsGame.get("trophyTitleIconUrl").getAsString());
+            game.setHasDlc(jsGame.get("hasTrophyGroups").getAsBoolean());
+
+            String platformName = jsGame.get("trophyTitlePlatform").getAsString();
+            Platform platform = Platform.valueOf(platformName.toUpperCase());
+            game.setPlatform(platform);
+
+            JsonObject trophiesInfo = jsGame.get("comparedUser").getAsJsonObject();
+            game.setBronze(trophiesInfo.get("bronze").getAsInt());
+            game.setSilver(trophiesInfo.get("silver").getAsInt());
+            game.setGold(trophiesInfo.get("gold").getAsInt());
+            game.setPlatinum(trophiesInfo.get("platinum").getAsInt());
+        }
+        return gameArray;
+    }
+
+    /**
+     *
+     * @param userProfile - User profile in JSON from server
+     * @return
+     */
     public Profile parseUserProfile(String userProfile) {
         Profile profile = new Profile();
         JsonObject jsonObject = new JsonParser().parse(userProfile).getAsJsonObject();
