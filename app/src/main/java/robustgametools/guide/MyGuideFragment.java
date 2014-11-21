efda486c77921fbc73491d6c9927d461f189d8b6
Kai <wngkai.91@gmail.com>
2014-11-20 20:50:40 -0800

    Fixed bug MyGuideFragment shows blank fragment after the first time

--- app/src/main/java/robustgametools/guide/MyGuideFragment.java
+++ app/src/main/java/robustgametools/guide/MyGuideFragment.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -96,7 +97,7 @@ public class MyGuideFragment extends Fragment {
         public void showGameGuide(TrophyGuide guide);
     }
 
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
