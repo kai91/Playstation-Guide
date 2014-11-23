decf32ebfd91c2dac1669acec46f46b1a7a77fe4
Kai <wngkai.91@gmail.com>
2014-11-23 09:17:18 -0800

    Updated GuideActivity to have drawer and updated its menu options

--- app/src/main/java/robustgametools/guide/GuideActivity.java
+++ app/src/main/java/robustgametools/guide/GuideActivity.java
@@ -1,27 +1,78 @@
 package robustgametools.guide;
 
-import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import robustgametools.adapter.NavigationDrawerAdapter;
+import robustgametools.model.BaseActivity;
+import robustgametools.model.TrophyGuide;
 import robustgametools.playstation_guide.R;
 
-public class GuideActivity extends Activity {
+public class GuideActivity extends BaseActivity {
+
+    @InjectView(R.id.drawer) DrawerLayout mDrawer;
+    @InjectView(R.id.drawer_menu) ListView mDrawerMenu;
+
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guide);
+        ButterKnife.inject(this);
+
         if (savedInstanceState == null) {
+            Fragment frag = new GuideFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("guideInfo", getTrophyGuideInfo());
+            frag.setArguments(bundle);
+
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, frag)
                     .commit();
         }
+
+        initDrawer();
+    }
+
+    private String getTrophyGuideInfo() {
+        Intent intent = getIntent();
+        String json = intent.getStringExtra("guideInfo");
+        return json;
+    }
+
+    private void initDrawer() {
+        setActionBarIcon(R.drawable.ic_drawer);
+        mDrawer.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawer,
+                mToolbar,
+                R.string.app_name,
+                R.string.app_name
+        );
+        mDrawer.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_guide;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 
 
@@ -34,29 +85,13 @@ public class GuideActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh_guide) {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_guide, container, false);
-            return rootView;
-        }
     }
 }
