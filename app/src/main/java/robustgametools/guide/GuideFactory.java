2e50538380e4bb3749180f64e40c1e9429730d5e
Kai <wngkai.91@gmail.com>
2014-11-27 06:30:36 -0800

    Add colours and the rest of text formatting

--- app/src/main/java/robustgametools/guide/GuideFactory.java
+++ app/src/main/java/robustgametools/guide/GuideFactory.java
@@ -1,9 +1,10 @@
 package robustgametools.guide;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
-import android.text.ParcelableSpan;
 import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -22,7 +23,7 @@ public class GuideFactory {
     private static GuideFactory mFormatter = null;
     private static Context mContext;
     private static ArrayList<View> views;
-    private boolean mIsOffline;
+    private static boolean mIsOffline;
 
     private static final String mImageRegex = "[D]";
     private static final String mTextBoldRegex = "[T]";
@@ -32,8 +33,15 @@ public class GuideFactory {
     private static final String mSilverRegex = "[S]";
     private static final String mBronzeRegex = "[B]";
 
+    private static final int mBronze = Color.parseColor("#cd7f32");
+    private static final int mSilver = Color.parseColor("#808080");
+    private static final int mGold = Color.parseColor("#ffd700");
+    private static final int mPlatinum = Color.parseColor("#a5b6ec");
 
-    private GuideFactory() {};
+    private GuideFactory() {
+    }
+
+    ;
 
     public static GuideFactory getInstance(Context context) {
         if (mFormatter == null) {
@@ -67,7 +75,7 @@ public class GuideFactory {
 
     private void formatImage(String rawGuide) {
         int index = rawGuide.indexOf(mImageRegex);
-        if (index  == -1) {
+        if (index == -1) {
             // no image in guide, proceed to extract text
             formatText(rawGuide);
         } else {
@@ -83,18 +91,20 @@ public class GuideFactory {
 
     private void formatText(String rawString) {
         SpannableStringBuilder builder = new SpannableStringBuilder(rawString);
-        rawString = formatRegex(builder, rawString, mTextBoldRegex, null);
-        //rawString = formatRegex(builder, rawString, mBronzeRegex, null);
+        rawString = formatBold(builder, rawString, mTextBoldRegex);
+        rawString = formatBronze(builder, rawString);
+        rawString = formatSilver(builder, rawString);
+        rawString = formatGold(builder, rawString);
+        rawString = formatPlatinum(builder, rawString);
         TextView text = new TextView(mContext);
         text.setText(builder);
         views.add(text);
     }
 
-    private String formatRegex(SpannableStringBuilder spannable,
-                               String string, String regex,
-                               ArrayList<ParcelableSpan> spans) {
+    private String formatBold(SpannableStringBuilder spannable,
+                              String string, String regex) {
         int start = string.indexOf(regex);
-        while(start != -1) {
+        while (start != -1) {
             string = string.replaceFirst(Pattern.quote(regex), "");
             int end = string.indexOf(regex);
             string = string.replaceFirst(Pattern.quote(regex), "");
@@ -106,10 +116,68 @@ public class GuideFactory {
         return string;
     }
 
-    private void extractButtons(String rawGuide) {
+    private String formatBronze(SpannableStringBuilder spannable,
+                                  String string) {
+        int start = string.indexOf(mBronzeRegex);
+        while (start != -1) {
+            string = string.replaceFirst(Pattern.quote(mBronzeRegex), "");
+            int end = string.indexOf(mBronzeRegex);
+            string = string.replaceFirst(Pattern.quote(mBronzeRegex), "");
+            String bold = string.substring(start, end);
+            spannable.replace(start, end + mBronzeRegex.length() * 2, bold);
+            spannable.setSpan(new StyleSpan(Typeface.BOLD), start, end, 0);
+            spannable.setSpan(new ForegroundColorSpan(mBronze), start, end, 0);
+            start = string.indexOf(mBronzeRegex);
+        }
+        return string;
     }
 
+    private String formatSilver(SpannableStringBuilder spannable,
+                              String string) {
+        int start = string.indexOf(mSilverRegex);
+        while (start != -1) {
+            string = string.replaceFirst(Pattern.quote(mSilverRegex), "");
+            int end = string.indexOf(mSilverRegex);
+            string = string.replaceFirst(Pattern.quote(mSilverRegex), "");
+            String bold = string.substring(start, end);
+            spannable.replace(start, end + mSilverRegex.length() * 2, bold);
+            spannable.setSpan(new StyleSpan(Typeface.BOLD), start, end, 0);
+            spannable.setSpan(new ForegroundColorSpan(mSilver), start, end, 0);
+            start = string.indexOf(mSilverRegex);
+        }
+        return string;
+    }
 
+    private String formatGold(SpannableStringBuilder spannable,
+                                String string) {
+        int start = string.indexOf(mGoldRegex);
+        while (start != -1) {
+            string = string.replaceFirst(Pattern.quote(mGoldRegex), "");
+            int end = string.indexOf(mGoldRegex);
+            string = string.replaceFirst(Pattern.quote(mGoldRegex), "");
+            String bold = string.substring(start, end);
+            spannable.replace(start, end + mGoldRegex.length() * 2, bold);
+            spannable.setSpan(new StyleSpan(Typeface.BOLD), start, end, 0);
+            spannable.setSpan(new ForegroundColorSpan(mGold), start, end, 0);
+            start = string.indexOf(mGoldRegex);
+        }
+        return string;
+    }
 
+    private String formatPlatinum(SpannableStringBuilder spannable,
+                              String string) {
+        int start = string.indexOf(mPlatinumRegex);
+        while (start != -1) {
+            string = string.replaceFirst(Pattern.quote(mPlatinumRegex), "");
+            int end = string.indexOf(mPlatinumRegex);
+            string = string.replaceFirst(Pattern.quote(mPlatinumRegex), "");
+            String bold = string.substring(start, end);
+            spannable.replace(start, end + mPlatinumRegex.length() * 2, bold);
+            spannable.setSpan(new StyleSpan(Typeface.BOLD), start, end, 0);
+            spannable.setSpan(new ForegroundColorSpan(mPlatinum), start, end, 0);
+            start = string.indexOf(mPlatinumRegex);
+        }
+        return string;
+    }
 
 }
